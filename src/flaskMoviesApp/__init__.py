450d66b70d7e7921fe4d3523b5fd90e7c73daf8d
Stavros Lagos <stavroslagos@gmail.com>
2022-02-03 22:18:54 +0200

    Changes to dunder init file

--- src/flaskMoviesApp/__init__.py
+++ src/flaskMoviesApp/__init__.py
@@ -12,24 +12,27 @@ app = Flask(__name__)
 ### Configuration για τα Secret Key, WTF CSRF Secret Key, SQLAlchemy Database URL, 
 ## Το όνομα του αρχείου της βάσης δεδομένων θα πρέπει να είναι 'flask_movies_database.db'
 
-
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 ### Αρχικοποίηση της Βάσης, και άλλων εργαλείων ###
 ### Δώστε τις σωστές τιμές παρακάτω ###
 
-db = ##
+app.config["SECRET_KEY"] = 'b668cbc68d29fd2b7f5976c54c39f6ec'
+app.config['WTF_CSRF_SECRET_KEY'] = 'fe9d487ba2c9a1f13a5d72fa0d76d3fb'
 
-bcrypt = ##
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask_movies_database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-login_manager = ##
+db = SQLAlchemy(app)
 
-login_manager.login_view = ##
-login_manager.login_message_category = ##
-login_manager.login_message = ##
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = "login"
+login_manager.login_message_category = "warning"
+login_manager.login_message = "Παρακαλούμε κάντε login για να μπορέσετε να δείτε αυτή τη σελίδα."
 
 
 from flaskMoviesApp import routes
